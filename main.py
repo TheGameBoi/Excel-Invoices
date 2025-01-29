4dded61e66844af1f9c15234951ca79351dcffe1
websites <websites.unpinned074@passinbox.com>
2025-01-29 17:25:40 -0600

    Added the Date to the PDF files.

--- main.py
+++ main.py
@@ -12,8 +12,18 @@ for filepath in filepaths:
     print(df)
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
-    filename = Path(filepath).stem + '.pdf'
+
+
+    filename = Path(filepath).stem
     invoice = filename.split('-')[0]
+
+    filename = Path(filepath).stem
+    date = filename.split('-')[1]
+
     pdf.set_font('Arial', 'B', 16)
-    pdf.cell(w=0, h=8, txt=f"Invoice Number {invoice}")
-    pdf.output(f"PDF/{filename}")
+    pdf.cell(w=0, h=8, txt=f"Invoice Number: {invoice}", ln=1)
+
+    pdf.set_font('Arial', 'B', 16)
+    pdf.cell(w=0, h=8, txt=f"Date: {date}")
+
+    pdf.output(f"PDF/{filename}.pdf")
