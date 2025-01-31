80e75e54e83b021602369efd7b718225f9e75ae6
websites <websites.unpinned074@passinbox.com>
2025-01-30 18:02:03 -0600

    Added the table to the PDF output.

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ filepaths = glob.glob('invoices/*.xlsx')
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name='Sheet 1')
-    print(df)
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
 
@@ -24,6 +23,29 @@ for filepath in filepaths:
     pdf.cell(w=0, h=8, txt=f"Invoice Number: {invoice}", ln=1)
 
     pdf.set_font('Arial', 'B', 16)
-    pdf.cell(w=0, h=8, txt=f"Date: {date}")
+    pdf.cell(w=0, h=8, txt=f"Date: {date}", ln=1)
+
+
+    df = pd.read_excel(filepath, sheet_name='Sheet 1')
+
+    columns = df.columns
+    columns = [item.replace('_', ' ').title() for item in columns]
+    pdf.set_font('Times', 'B', 9)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=70, h=8, txt=columns[1], border=1)
+    pdf.cell(w=30, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+
+    for index, row in df.iterrows():
+        pdf.set_font('Arial', 'B', 10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
 
     pdf.output(f"PDF/{filename}.pdf")
